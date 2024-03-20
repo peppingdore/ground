324738b167718906c0bd159049b0d5c6ef6d709e
peppingdore <butichex@gmail.com>
2024-03-20 22:23:51 +0300

    ScopeRestore

--- scoped.h
+++ scoped.h
@@ -7,3 +7,13 @@ struct ScopedLockImpl {
 	~ScopedLockImpl() { lock.unlock(); }
 };
 #define ScopedLock(lock) ScopedLockImpl CONCAT(__scoped_lock, __LINE__)(lock);
+
+template <typename T>
+struct ScopedRestoreImpl {
+	T& dst;
+	T  value;
+
+	ScopedRestore(T& dst): dst(dst), value(dst) {};
+	~ScopedRestore() { dst = value; };
+};
+#define ScopedRestore(x) ScopedRestoreImpl CONCAT(__scoped_restore, __LINE__)(x);
