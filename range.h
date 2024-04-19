db96f45e0b2468999fa97715c03af24de7a1a9dc
peppingdore <butichex@gmail.com>
2024-04-19 12:27:24 +0300

    simplify range

--- range.h
+++ range.h
@@ -1,85 +1,35 @@
 #pragma once
 
 #include "base.h"
+#include "math/basic_functions.h"
 
-template <typename T = s64, bool Ascending = true>
-struct NumericRange {
-	T current;
-	T target;
+struct Range {
+	s64 start;
+	s64 target;
 
-	constexpr void operator++() {
-		current += (Ascending ? 1 : -1);
-	}
-
-	constexpr void operator--() {
-		current += (Ascending ? -1 : 1);
-	}
-
-	constexpr operator bool() {
-		return target != current;
-	}
-
-	constexpr bool operator!=(NumericRange other) {
-		return (bool) *this;
-	}
-
-	constexpr T operator*() {
-		if constexpr (Ascending) {
-			return current;
-		} else {
-			return current - 1;
-		}
-	}
-
-	constexpr NumericRange& begin() {
-		return *this;
-	}
-
-	constexpr NumericRange& end() {
-		return *this;
-	}
-
-	constexpr NumericRange<T, !Ascending> reverse() {
-		return {
-			.current = target,
-			.target  = current,
-		};
-	}
+	Range begin() { return *this; }
+	Range end() { return *this; }
+	void  operator++() { start += sign(target - start); }
+	bool  operator!=(Range other) { return start != other.target; }
+	s64   operator*() { return start; }
 };
 
-template <typename T>
-constexpr auto range(T start, T count) {
-	return NumericRange<T> {
-		.current = start,
-		.target  = (T) (start + count),
-	};
+Range range(s64 start, s64 count) {
+	return { start, start + count };
 }
 
-template <typename T>
-constexpr auto range(T count) {
-	return range<T>(0, count);
+Range range(s64 count) {
+	return { 0, count };
 }
 
-constexpr auto range(s64 start, s64 count) {
-	assert(count >= 0);
-
-	NumericRange<> r = {
-		.current = start,
-		.target  = start + count,
-	};
-
-	return r;
+Range range_from_to(s64 start, s64 end_exclusive) {
+	return { start, end_exclusive };
 }
 
-constexpr auto range(s64 count) {
-	return range(0, count);
-}
-
-constexpr auto range_from_to(s64 start, s64 last_index_exclusive) {
-	return range(start, last_index_exclusive - start);
-}
-
-template <typename T = s64, bool Ascending = true>
-constexpr auto reverse(NumericRange<T, Ascending> range) {
-	return range.reverse();
+Range reverse(Range range) {
+	if (range.start < range.target) {
+		return { range.target - 1, range.start - 1 };
+	} else {
+		return { range.target + 1, range.start + 1 };
+	}
 }
