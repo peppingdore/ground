ab502a4edf58c729d0dac87c50e18418f050fd53
peppingdore <butichex@gmail.com>
2024-04-22 17:11:32 +0300

    one dimensional ranges patching

---
+++ one_dim_intersect_test.cpp
@@ -0,0 +1,90 @@
+#include "testing.h"
+#include "one_dim_intersect.h"
+#include "array.h"
+#include "format.h"
+#include "defer.h"
+
+struct TestRegion {
+	s64  start;
+	s64  length;
+	char c;
+
+	REFLECT(TestRegion) {
+		MEMBER(start);
+		MEMBER(length);
+		MEMBER(c);
+	}
+};
+
+void verify_one_dim_array(Array<TestRegion> regions, String str, CodeLocation loc = caller_loc()) {
+	tester_scope_push(loc);
+	defer { tester_scope_pop(); };
+
+	s64 last_end = 0;
+	for (auto it: regions) {
+		EXPECT(it.length > 0);
+		EXPECT(last_end == it.start);
+		last_end = it.start + it.length;
+	}
+
+	auto sb = build_string();
+	for (auto it: regions) {
+		for (auto i: range(it.length)) {
+			sb.append(it.c);
+		}
+	}
+	EXPECT(sb.get_string() == str);
+	println(sb.get_string());
+}
+
+Array<TestRegion> build_test_regions() {
+	Array<TestRegion> regions;
+	regions.add(TestRegion{0,  10, 'a'});
+	regions.add(TestRegion{10, 10, 'B'});
+	regions.add(TestRegion{20, 10, 'c'});
+	regions.add(TestRegion{30, 10, 'D'});
+	return regions;
+}
+
+TEST(one_dim) {
+	auto regions = build_test_regions();
+	
+	auto get_region = [&](s64 i) {
+		auto reg = *regions[i];
+		return OneDimRegion { reg.start, reg.length };
+	};
+
+	auto resize = [&](s64 i, s64 start, s64 end) {
+		regions[i]->start = start;
+		regions[i]->length = end - start;
+	};
+
+	char insert_letter = 'N';
+	auto insert = [&](s64 i, s64 start, s64 end, s64 copy_idx) {
+		if (copy_idx == -1) {
+			regions.add_at_index(i, TestRegion{start, end - start, insert_letter});
+		} else {
+			regions.add_at_index(i, TestRegion{start, end - start, regions[copy_idx]->c });
+		}
+	};
+
+	auto remove = [&](s64 i) {
+		regions.remove_at_index(i);
+	};
+
+	verify_one_dim_array(regions, "aaaaaaaaaaBBBBBBBBBBccccccccccDDDDDDDDDD"_b);
+	one_dim_patch(len(regions), get_region, resize, insert, remove, 3, 30);
+	verify_one_dim_array(regions, "aaaNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDD"_b);
+	insert_letter = 'L';
+	one_dim_patch(len(regions), get_region, resize, insert, remove, 6, 10);
+	verify_one_dim_array(regions, "aaaNNNLLLLLLLLLLNNNNNNNNNNNNNNNNNDDDDDDD"_b);
+	insert_letter = 'M';
+	one_dim_patch(len(regions), get_region, resize, insert, remove, 20, 4);
+	verify_one_dim_array(regions, "aaaNNNLLLLLLLLLLNNNNMMMMNNNNNNNNNDDDDDDD"_b);
+	insert_letter = 'P';
+	one_dim_patch(len(regions), get_region, resize, insert, remove, 1, 38);
+	verify_one_dim_array(regions, "aPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPD"_b);
+	insert_letter = 'O';
+	one_dim_patch(len(regions), get_region, resize, insert, remove, 0, 40);
+	verify_one_dim_array(regions, "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"_b);
+}
