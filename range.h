203f743cdd58e184c25b984d20f5b3a5cc5fc001
peppingdore <butichex@gmail.com>
2024-08-28 03:18:20 +0300

    now range_* function series only allow ascending ranges.

--- range.h
+++ range.h
@@ -14,16 +14,16 @@ struct Range {
 	s64   operator*() { return start; }
 };
 
+Range range_from_to(s64 start, s64 end_exclusive) {
+	return { start, max_s64(start, end_exclusive) };
+}
+
 Range range(s64 start, s64 count) {
-	return { start, start + count };
+	return { start, max_s64(start, start + count) };
 }
 
 Range range(s64 count) {
-	return { 0, count };
-}
-
-Range range_from_to(s64 start, s64 end_exclusive) {
-	return { start, end_exclusive };
+	return { 0, max_s64(0, count) };
 }
 
 Range reverse(Range range) {
