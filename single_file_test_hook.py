bb7e3ebf391e1e63b0255a8bdee14af560d443aa
peppingdore <butichex@gmail.com>
2024-11-17 03:09:32 +0300

    single_file_test_hook.py now also combines everything in one file

--- single_file_test_hook.py
+++ single_file_test_hook.py
@@ -16,7 +16,7 @@ GRD_TEST(single_file_compilation) {{
 
 path = Path(__file__).parent / "single_file_compilation_tests"
 path.mkdir(parents=True, exist_ok=True)
-to_build = []
+all_files = []
 
 blacklist = ['.git', '.vscode', 'third_party', 'b_lib', 'testing.h', 'misc']
 
@@ -28,17 +28,33 @@ def gen(dir, level=1):
 			gen(dir / it.name, level=level+1)
 			continue
 		if it.name.endswith(".h"):
-			skiplist = ['posix_', 'darwin_'] if os.name == 'nt' else ['win_']
+			skiplist = (['posix_', 'darwin_'] if os.name == 'nt' else ['win_']) + ["_test"]
 			if any (x in it.name for x in skiplist):
 				continue
 			file_path = (path / dir / f'{it.name}_test.cpp').resolve()
 			file_path.parent.mkdir(parents=True, exist_ok=True)
-			to_build.append(file_path)
+			all_files.append(str(PurePosixPath("../") / dir / it.name))
 			file_path.write_text(CPP_CONTENT.format(
 				testing_header=str(PurePosixPath('../' * level) / "grd_testing.h"),
-				file=str(PurePosixPath('../' * level) / dir / it.name))
-			)
+				file=str(PurePosixPath('../' * level) / dir / it.name),
+			))
+
+# Useful for compilation time analysis
+def build_all_files():
+	filtered = filter(lambda x: all(not skip in x for skip in ["ssa.h", "grdc_parser.h", "ast_printer.h", "window.h"]) , all_files)
+	HEADER = """#pragma once
+#if 0
+	`dirname "$0"`/../build.sh $0 $@; exit
+#endif
+"""
+	FOOTER = """
+	int main() {
+		return 0;
+	}
+"""
+	(path / "all_file_compilation_test.cpp").write_text(HEADER + '\n'.join(map(lambda x: '#include "' + x + '"', filtered)) + FOOTER)
 
 def run_hook(tester):
 	shutil.rmtree(path, ignore_errors=True)
 	gen(Path('.'))
+	build_all_files()
