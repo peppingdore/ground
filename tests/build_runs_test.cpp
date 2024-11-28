b9ebee51827d3451b275d0444ba982dd989ce4e8
peppingdore <butichex@gmail.com>
2024-11-28 06:03:14 +0300

    build_run_test passes

--- tests/build_runs_test.cpp
+++ tests/build_runs_test.cpp
@@ -1,12 +1,16 @@
+#if 0
+	`dirname "$0"`/../build.sh $0 $@; exit
+#endif
 #include "../grd_build.h"
 #include "../grd_testing.h"
+#include "../grd_format.h"
 
 GRD_TEST_CASE(build_run_simple) {
-	GRD_BUILD_RUN("params.add_define((BUILD_RUN_SIMPLE, 5816431927631))")
+	GRD_BUILD_RUN("params.add_define(('BUILD_RUN_SIMPLE', 5816431927631))")
 	GRD_EXPECT_EQ(BUILD_RUN_SIMPLE, 5816431927631)
 }
 
 GRD_TEST_CASE(build_run_raw_literal) {
-	GRD_BUILD_RUN(R"(params.add_define((BUILD_RUN_RAW_LITERAL, 5816431927631)))")
+	GRD_BUILD_RUN(R"(params.add_define(('BUILD_RUN_RAW_LITERAL', 5816431927631)))")
 	GRD_EXPECT_EQ(BUILD_RUN_RAW_LITERAL, 5816431927631)
 }
