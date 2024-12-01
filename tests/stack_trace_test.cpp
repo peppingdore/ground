d8107a9d1721e3deca6cd5b6b7a6dde0490243bd
peppingdore <butichex@gmail.com>
2024-12-01 19:50:06 +0300

    complete test

--- tests/stack_trace_test.cpp
+++ tests/stack_trace_test.cpp
@@ -6,9 +6,25 @@
 #include "../grd_stack_trace.h"
 #include "../grd_log.h"
 #include "../grd_testing.h"
+#include "../grd_string.h"
+#include "../grd_format.h"
+
+GrdStackTrace deep_func() {
+	return grd_get_stack_trace();
+}
+
+GrdStackTrace func() {
+	return deep_func();
+}
 
 GRD_TEST_CASE(stack_trace) {
-	auto cs = grd_get_stack_trace();
-	grd_print_stack_trace(cs);
-	GRD_EXPECT(false);
+	auto st = func();
+	char buf[4 * 1024];
+	buf[0] = '\0';
+	grd_print_stack_trace(st, buf, sizeof(buf));
+	auto str = grd_make_string(buf);
+	grd_println(str);
+	GRD_EXPECT(grd_contains(str, "func"), str);
+	GRD_EXPECT(grd_contains(str, "deep_func"), str);
+	GRD_EXPECT(grd_contains(str, "main"), str);
 }
