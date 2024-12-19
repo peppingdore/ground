5f16e1077223dfbed7e8217cea0d51848ad50ff2
peppingdore <butichex@gmail.com>
2024-12-19 11:22:11 +0300

    do not use hook, in build_subproj

--- build_subproj.py
+++ build_subproj.py
@@ -35,28 +35,25 @@ def need_to_build(name):
 	args, _ = parser.parse_known_args()
 	return args.subprojects != "none" and (args.subprojects == "all" or (name in args.subprojects.split(',')))
 
-def cmake(ctx, root, *, opts=None, build_type=None, callback=None):
+def cmake(ctx, root, *, opts=None, build_type=None):
 	opts = opts or []
-	def build_hook(ctx):
-		nonlocal root
-		nonlocal build_type
-		import argparse
-		parser = argparse.ArgumentParser()
-		parser.add_argument("--subprojects", default="all")
-		args, _ = parser.parse_known_args()
-		root = Path(root).resolve()
-		did_build = False
-		name = root.name
-		if not name:
-			raise Exception("root path must be a normal directory")
-		build = Path(root).parent / "grd_cmake_build" / name
-		build_type = build_type or pick_cmake_build_type(ctx)
-		if args.subprojects != "none" and (args.subprojects == "all" or (name in args.subprojects.split(','))):
-			print(f"{Path(__file__).name}: building {root}\n{Path(__file__).name}: build type: {build_type}")
-			os.makedirs(build, exist_ok=True)
-			subprocess.check_call(["cmake", *opts, "-DCMAKE_CXX_STANDARD=23", f"-DCMAKE_BUILD_TYPE={build_type}", ".", f"-B{build.resolve()}"], cwd=root)
-			subprocess.check_call(["cmake",  "--build", str(build.resolve()), f'--config {build_type}'], cwd=root)
-			did_build = True
-		if callback:
-			callback(ctx, build_path=build.resolve(), build_type=build_type, did_build=did_build)
-	ctx.add_pre_compile_hook(build_hook)
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--subprojects", default="all")
+	args, _ = parser.parse_known_args()
+	root = Path(root).resolve()
+	did_build = False
+	name = root.name
+	if not name:
+		raise Exception("root path must be a normal directory")
+	build = Path(root).parent / "grd_cmake_build" / name
+	build_type = build_type or pick_cmake_build_type(ctx)
+	if args.subprojects != "none" and (args.subprojects == "all" or (name in args.subprojects.split(','))):
+		print(f"{Path(__file__).name}: building {root}\n{Path(__file__).name}: build type: {build_type}")
+		os.makedirs(build, exist_ok=True)
+		subprocess.check_call(["cmake", *opts, "-DCMAKE_CXX_STANDARD=23", f"-DCMAKE_BUILD_TYPE={build_type}", ".", f"-B{build.resolve()}"], cwd=root)
+		subprocess.check_call(["cmake",  "--build", str(build.resolve()), f'--config {build_type}'], cwd=root)
+		did_build = True
+	from collections import namedtuple
+	tp = namedtuple("BuildResult", ["build_path", "build_type", "did_build"])
+	return tp(build_path=build.resolve(), build_type=build_type, did_build=did_build)
