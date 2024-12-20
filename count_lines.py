bffb46ee124feb5aa95cbcd4144411a0bc0e5ea9
peppingdore <butichex@gmail.com>
2024-12-20 10:49:25 +0300

    count_lines.py: ignore more stuff

--- count_lines.py
+++ count_lines.py
@@ -3,4 +3,4 @@ import os
 import subprocess
 from pathlib import Path
 
-subprocess.run(f"cloc {Path(__file__).parent} --exclude-dir=third_party,single_file_compilation_tests,old_gpu,b_lib,gen --exclude-ext=txt --by-file", shell=True)
+subprocess.run(f"cloc {Path(__file__).parent} --exclude-dir=third_party,single_file_compilation_tests,old_gpu,b_lib,gen --exclude-ext=txt,json --by-file", shell=True)
