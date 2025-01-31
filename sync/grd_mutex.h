faafbea7d2db1613f63f41f42f3e85ea60d4c598
peppingdore <butichex@gmail.com>
2025-01-31 22:37:58 +0300

    GrdMutex: use proc instead of method

--- sync/grd_mutex.h
+++ sync/grd_mutex.h
@@ -6,18 +6,18 @@
 struct GrdMutex {
 	GrdOsMutex os_mutex;
 
-	void lock() {
-		grd_os_mutex_lock(&os_mutex);
-	}
-
-	void unlock() {
-		grd_os_mutex_unlock(&os_mutex);
-	}
-
 	void free() {
 		grd_os_mutex_destroy(&os_mutex);
 	}
 };
+
+GRD_DEF grd_lock(GrdMutex* mutex) {
+	grd_os_mutex_lock(&mutex->os_mutex);
+}
+
+GRD_DEF grd_unlock(GrdMutex* mutex) {
+	grd_os_mutex_unlock(&mutex->os_mutex);
+}
 
 GRD_DEDUP void grd_make_mutex(GrdMutex* out_mutex) {
 	grd_os_mutex_create(&out_mutex->os_mutex);
