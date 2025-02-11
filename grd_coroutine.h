c8690e178941722ccd5eb963ed74d03b94191e72
peppingdore <butichex@gmail.com>
2025-02-11 12:12:45 +0300

    fix error

--- grd_coroutine.h
+++ grd_coroutine.h
@@ -140,16 +140,9 @@ GRD_DEDUP int end(GrdHasGlobalGeneratorIterator auto t) {
 	return 0;
 }
 
-#if !GRD_COMPILER_MSVC
-GRD_DEDUP auto grd_map(auto iter, auto func) -> GrdGenerator<decltype(func(*iter.begin()))> {
+template <typename T, typename V> // Can't use |auto|, MSVC bitches.  
+GRD_DEF grd_map(T iter, V func) -> GrdGenerator<decltype(func(*begin(iter)))> {
 	for (auto it: iter) {
 		co_yield func(it);
 	}
 }
-
-GRD_DEDUP auto grd_map(auto iter, auto func) -> GrdGenerator<decltype(func(*begin(iter)))> {
-	for (auto it: iter) {
-		co_yield func(it);
-	}
-}
-#endif
